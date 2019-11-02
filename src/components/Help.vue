401c5a8291b046cde25916f5ff0e795b3ecf12f7
Daniel Jakob <wolverine@usox.org>
2019-11-02 12:07:49 +0100

    Update urls

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -4,10 +4,10 @@
 			<div class="box--header" v-html="$t('help.title')"></div>
 			<div class="help--content" v-html="$t('help.content')"></div>
 			<div>
-				<a href="https://discord.gg/PnnGdXD" target="_blank" rel="noreferrer" class="box--link">Discord</a>
+				<a href="http://chat.stuniverse.de" target="_blank" rel="noreferrer" class="box--link">Discord</a>
 			</div>
 			<div>
-				<a href="https://stu.wolvnet.de/docu" target="blank" rel="noreferrer" class="box--link">{{ $t("help.documentation") }}</a>
+				<a href="https://docu.stuniverse.de" target="blank" rel="noreferrer" class="box--link">{{ $t("help.documentation") }}</a>
 			</div>
 		</div>
 	</HomeLayout>
